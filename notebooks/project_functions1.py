61fd8de8861b4d3de34dd3e45a0e1859924ab350
LLyrilusc <ytxie_michael@outlook.com>
2021-11-01 04:25:31 +0800

    Michael's notebook finished. One more function was added into Michael's project function.

--- notebooks/project_functions1.py
+++ notebooks/project_functions1.py
@@ -10,6 +10,20 @@ def load_and_process(url_or_path_to_csv_file):
     df[df ==".."] = np.nan
     df[df =="..t"] = np.nan
     df_cleaned = df.dropna(axis=0)
-    df = df_cleaned.T
-    return df
+    df1 = df_cleaned.T
+    return df1
 
+def mean_sd_calculation(dataframe):
+    dic = dict(dataframe)
+    del dic["Date"]
+    d = {"Date":("Mean","Standard Deviation")}
+  
+    for key in dic:
+        mean = (sum(dic[key])/len(dic[key]))
+        sd = np.std(dic[key])
+        d[key]= [mean,sd]
+    df2 = pd.DataFrame.from_dict(d)
+    right = dataframe
+    left = df2
+    result = pd.merge(right,left,how="outer")
+    return result
