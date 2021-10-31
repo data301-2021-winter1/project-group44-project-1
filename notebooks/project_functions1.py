bbfb9ad846ea94dc75dffc89457a5d89b2d5f808
LLyrilusc <ytxie_michael@outlook.com>
2021-11-01 02:28:07 +0800

    Michael make changes to the dataset

--- notebooks/project_functions1.py
+++ notebooks/project_functions1.py
@@ -1,10 +1,15 @@
 #Michael's Functions
 import pandas as pd
 import numpy as np
+
+
+
 def load_and_process(url_or_path_to_csv_file):
     df = pd.read_csv(url_or_path_to_csv_file)
+    df = df.T
     df[df ==".."] = np.nan
     df[df =="..t"] = np.nan
     df_cleaned = df.dropna(axis=0)
-    df_cleaned = df_cleaned.T
-    return df_cleaned
+    df = df_cleaned.T
+    return df
+
